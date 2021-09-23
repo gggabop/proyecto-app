6a015bac465269f184b6a7857bea66a89662ff9f
Jesus <jesusperez@seocontenidos.net>
2021-09-23 16:49:34 -0400

    status cash order in 0

--- database/migrations/2021_08_07_192001_create_cash_orders_table.php
+++ database/migrations/2021_08_07_192001_create_cash_orders_table.php
@@ -17,7 +17,7 @@ class CreateCashOrdersTable extends Migration
             $table->id();
             $table->foreignId('fk_customer_id')->references('id')->on('customers');
             $table->double('amount_cash_order',20,2);
-            $table->integer('status_cash_order');
+            $table->integer('status_cash_order')->default(0);
             $table->integer('register_status_db_cashOrder')->default(0);
             $table->timestamps();
         });
